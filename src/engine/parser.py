cfd2d5aed04fbb4493923a69098deb5c69781b8e
Anant Bhardwaj <anantb@csail.mit.edu>
2012-12-09 01:42:50 -0500

    all works perfect

--- src/engine/parser.py
+++ src/engine/parser.py
@@ -15,16 +15,30 @@ def stanford_parse_local(sen):
 	out, err = p.communicate()
 	return str(out.strip())
 	
-	
 
-def stanford_parse_web(params, url='/parser/parser.jsp'):
-	conn = httplib.HTTPConnection("anantb.csail.mit.edu:8080")
+
+def propbank_parse_web(sen, url='/parse'):
+	conn = httplib.HTTPConnection('barbar.cs.lth.se:8081')
 	headers = {"Content-type": "application/x-www-form-urlencoded",
 			"Accept": "text/plain"}
+	params = {'sentence':sen, 'returnType':'text', 'doRenderDependencyGraph':'on'}
 	params = urllib.urlencode(params)
 	conn.request("POST", url, params, headers)	
 	res = conn.getresponse().read()
 	return res	
 	
+
+def stanford_parse_web(sen, url='/parser/parser.jsp'):
+	conn = httplib.HTTPConnection("anantb.csail.mit.edu:8080")
+	headers = {"Content-type": "application/x-www-form-urlencoded",
+			"Accept": "text/plain"}
+	params = {'input':sen}
+	params = urllib.urlencode(params)
+	conn.request("POST", url, params, headers)	
+	res = conn.getresponse().read().strip()
+	return res	
+	
 if __name__ == "__main__":
 	print stanford_parse_local('I hate NLP')
+	print propbank_parse_web('I hate NLP')
+	print stanford_parse_web('I hate NLP')
