aa41916063fec91baf22265895f473ec35b7ef40
Anant Bhardwaj <anantb@slow.csail.mit.edu>
2012-12-09 22:24:32 -0500

    add module

--- src/server/settings.py
+++ src/server/settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'server',
-	'engine',
+    'engine',
 )
 
 # A sample logging configuration. The only tangible logging
